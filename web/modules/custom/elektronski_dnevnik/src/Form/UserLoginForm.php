764a7dbba31c1a62bed61c0580cef82c1a97c0b8
stojk3 <stojkebrd@gmail.com>
2025-02-22 18:29:38 +0100

    Napravljena login stranica i baza jos malo doradjena

---
+++ web/modules/custom/elektronski_dnevnik/src/Form/UserLoginForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\elektronski_dnevnik\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Database;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class UserLoginForm extends FormBase {
+
+  /**
+   * ID forme
+   */
+  public function getFormId() {
+    return 'elektronski_dnevnik_login_form';
+  }
+
+  /**
+   * Build Form
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['username'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Username'),
+      '#required' => TRUE,
+    ];
+
+    $form['password'] = [
+      '#type' => 'password',
+      '#title' => $this->t('Password'),
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Login'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Submit handler
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $username = $form_state->getValue('username');
+    $password = $form_state->getValue('password');
+
+    $connection = Database::getConnection();
+    $tables = ['teachers', 'students'];
+
+    foreach ($tables as $table) {
+      $query = $connection->select($table, 'u')
+        ->fields('u', ['id'])
+        ->condition('username', $username)
+        ->condition('password', $password)
+        ->execute()
+        ->fetchField();
+
+      if ($query) {
+        \Drupal::messenger()->addMessage($this->t('Login successful!'));
+        $response = new RedirectResponse('/pocetna'); 
+        $response->send();
+        return;
+      }
+    }
+
+    \Drupal::messenger()->addError($this->t('Invalid username or password.'));
+  }
+}
