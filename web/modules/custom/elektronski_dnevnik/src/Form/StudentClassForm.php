dfbcb441659e4da90c0b0971f006c2ef2276c0f9
H4ze123 <ilij.stoj@gmail.com>
2025-03-04 11:09:28 +0100

    Pocetak upisivanja casova

--- web/modules/custom/elektronski_dnevnik/src/Form/StudentClassForm.php
+++ web/modules/custom/elektronski_dnevnik/src/Form/StudentClassForm.php
@@ -11,4 +11,17 @@ class StudentClassForm extends FormBase {
   public function getFormId() {
     return 'student_class_form';
   }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['datum_upisa'] = [
+        '#type' => 'date',
+        '#title' => t('Datum upisa'),
+        '#default_value' => date('Y-m-d'),
+        '#required' => TRUE,
+        '#ajax' => [
+            'callback' => '::updateWeekAndClasses',
+            'wrapper' => 'class-info-container',
+        ],
+    ];
+  }
 }
