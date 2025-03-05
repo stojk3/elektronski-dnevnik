521948bb10c68babb48a28ed9a60b0ec106bcf6b
H4ze123 <ilija.stojsin@gmail.com>
2025-03-05 17:19:43 +0100

    Presek stanja da se nesto nedo bog ne kresovalo

--- web/modules/custom/elektronski_dnevnik/src/Form/StudentClassForm.php
+++ web/modules/custom/elektronski_dnevnik/src/Form/StudentClassForm.php
@@ -46,5 +46,94 @@ class StudentClassForm extends FormBase {
 
     $current_user = \Drupal::currentUser();
     $connection = \Drupal::database();
+    $user_username = $current_user->getAccountName();
+
+    $query = $connection->select('teachers', 't')
+        ->fields('t', ['subject_id'])
+        ->condition('t.username', $user_username, '=')
+        ->execute()
+        ->fetchCol();
+      
+    if (!empty($query)) {
+        $subjects_id = $query;
+
+        $subjects_query = $connection->select('subject', 's')
+                ->fields('s', ['id', 'predmet'])
+                ->condition('s.id', $subjects_id, 'IN')
+                ->execute();
+
+        $subjects = [];
+        foreach ($subjects_query as $row) {
+            $subjects[$row->id] = t($row->predmet);
+        }
+
+        $form['predmet'] = [
+            '#type' => 'select',
+            '#title' => t('Predmet'),
+            '#options' => $subjects,
+            '#required' => TRUE,
+            '#ajax' => [
+                'callback' => '::updateCombinedContainer',
+                'wrapper' => 'combined-container',
+            ],
+        ];
+    } else {
+        $form['message'] = [
+            '#markup' => t('Nema predmeta blablabla'),
+        ];
+    }
+
+    $form['odeljenje'] = [
+        '#type' => 'select',
+        '#title' => t('Odeljenje'),
+        '#options' => [
+            'I1' => t('I1'),
+            'I2' => t('I2'),
+            'I3' => t('I3'),
+            'II1' => t('II1'),
+            'II2' => t('II2'),
+            'II3' => t('II3'),
+            'III1' => t('III1'),
+            'III2' => t('III2'),
+            'III3' => t('III3'),
+            'IV1' => t('IV1'),
+            'IV2' => t('IV2'),
+            'IV3' => t('IV3'),
+        ],
+        '#required' => TRUE,
+        '#ajax' => [
+            'callback' => '::updateCombinedContainer',
+            'wrapper' => 'combined-container',
+        ],
+    ];
+
+    $form['combined-container'] = [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'combined-container'],
+    ];
+
+    $total_classes = $this->getTotalClassesForSubjectAndClass(
+        $form_state->getValue('predmet'),
+        $form_state->getValue('odeljenje'),
+    );
+
+    $form['combined-container']['ukupno_casova'] = [
+        '#type' => 'number',
+        '#title' => t('Ukupno CHASOVA(zameni ovde sa ch)'),
+        '#default_value' => $total_classes,
+        '#required' => TRUE,
+        '#disabled' => TRUE,
+    ];
+
+    $selected_class = $form_state->getValue('odeljenje');
+    $students = $this->loadStudentByClass($selected_class);
+
+    return $form;
+  }
+
+  protected function getWeekNumberFromDate($date) {
+    $first_week_date = '2024-09-01';
+    $date_diff = (strtotime($date) - strtotime($first_week_date)) / (60 *60 *24 * 7);
+    return ceil($date_diff) + 1;
   }
 }
